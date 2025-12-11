7254cb7974e668c74660df6732e2cacce6f801f3
m263624 <m263624@usna.edu>
2025-12-11 16:34:16 -0500

    data_collect.py

---
+++ data_collect_backup.py
@@ -0,0 +1,43 @@
+# https://mediastack.com/documentation
+
+# // Live News Data
+
+# http://api.mediastack.com/v1/news
+#     ? access_key = YOUR_ACCESS_KEY
+    
+# // optional parameters: 
+
+#     & sources = cnn,bbc
+#     & categories = business,sports 
+                    # general - Uncategorized News
+                    # business - Business News
+                    # entertainment - Entertainment News
+                    # health - Health News
+                    # science - Science News
+                    # sports - Sports News
+                    # technology - Technology News
+#     & countries = us,au
+#     & languages = en,-de
+#     & keywords = virus,-corona
+#     & sort = published_desc
+#     & offset = 0
+#     & limit = 100
+
+
+import http.client, urllib.parse
+
+conn = http.client.HTTPConnection('api.mediastack.com')
+
+params = urllib.parse.urlencode({
+    'access_key': 'ACCESS_KEY',
+    'categories': '-general,-sports',
+    'sort': 'published_desc',
+    'limit': 10,
+    })
+
+conn.request('GET', '/v1/news?{}'.format(params))
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode('utf-8'))
